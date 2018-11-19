1165d0e1d53a1c19eeacff631efa84e4bb2fcb74
NasaSugisawa <36944085+NasaSugisawa@users.noreply.github.com>
2018-11-19 13:53:06 +0900

    project作成

---
+++ src/main/java/com/egwms/example/samplehttps/SampleHttpsApplication.java
@@ -0,0 +1,12 @@
+package com.egwms.example.samplehttps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleHttpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleHttpsApplication.class, args);
+	}
+}
